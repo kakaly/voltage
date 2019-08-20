d033c5f70f58f2de9063677c0f22762e6b95e557
kakaly <reach.kkarthik@gmail.com>
2019-08-19 22:46:20 -0700

    Minor formatting

--- src/Containers/Results.jsx
+++ src/Containers/Results.jsx
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 import CategoryDropdown from '../Components/CategoryDropdown'
 import Producers from '../Components/Producers'
 
-const futureBass = ['Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.', 'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.', 'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.', 'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.', 'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.', 'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.' ]
+const futureBass = [
+  'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.',
+  'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.',
+  'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.',
+  'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.',
+  'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.',
+  'Simon White, better known as Duskus is a British, London-based electronic music producer and DJ. He is known for his cute sound and formant shifted vocal processing.'
+]
 
 const bassHouse = [
-      'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
-      'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
-      'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
-      'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
-      'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
-      'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.'
-    ]
+  'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
+  'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
+  'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
+  'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
+  'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.',
+  'With an eclectic mix of influences, Born Dirty has created a signature hybrid sound resting on a solid backbone of Hip-Hop, House and Techno.'
+]
 
 export default class Results extends Component {
 
@@ -26,10 +33,8 @@ export default class Results extends Component {
         }
     }
 
-
-
-
     render() {
+        console.log("inside Results:",this.props.genreIndex)
         return (
             <div className='results'>
               <CategoryDropdown changeCategoryHandler={this.props.changeCategoryHandler}
