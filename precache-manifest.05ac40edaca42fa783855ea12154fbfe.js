b198de7369571f43d9825c73e8b3d008ff57193a
charlesscheuer <cscheuer@wustl.edu>
2019-09-05 14:53:19 -0500

    Updates

---
+++ precache-manifest.05ac40edaca42fa783855ea12154fbfe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6656c531bdbdcf6a4102f4005a04eb84",
+    "url": "www.thevoltage.club/index.html"
+  },
+  {
+    "revision": "d371d304b85d2ea11038",
+    "url": "www.thevoltage.club/static/css/main.5693beb9.chunk.css"
+  },
+  {
+    "revision": "c36051ce43fc81565394",
+    "url": "www.thevoltage.club/static/js/2.1141488d.chunk.js"
+  },
+  {
+    "revision": "d371d304b85d2ea11038",
+    "url": "www.thevoltage.club/static/js/main.accf8b23.chunk.js"
+  },
+  {
+    "revision": "a7a3d41bb525642a92cf",
+    "url": "www.thevoltage.club/static/js/runtime~main.61e92061.js"
+  },
+  {
+    "revision": "0a92d79b511d5cb3850d1121074ed5f5",
+    "url": "www.thevoltage.club/static/media/BasisGrotesque.0a92d79b.woff"
+  },
+  {
+    "revision": "62cbdba6a7531bf78b96b300d7633885",
+    "url": "www.thevoltage.club/static/media/basis-grotesque-bold.62cbdba6.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/voltage/precache-manifest.1d63937a762d09b06650f401308deec3.js"
+  "www.thevoltage.club/precache-manifest.05ac40edaca42fa783855ea12154fbfe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/voltage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("www.thevoltage.club/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
